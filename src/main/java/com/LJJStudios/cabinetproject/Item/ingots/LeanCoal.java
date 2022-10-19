8b52657419588772c59d6976ee48af799eeb3cb6
jiajie.lei <leijiajie.ljj@qq.com>
2022-10-19 08:17:06 +0800

    update

--- src/main/java/com/LJJStudios/cabinetproject/Item/ingots/LeanCoal.java
+++ src/main/java/com/LJJStudios/cabinetproject/Item/ingots/LeanCoal.java
@@ -3,7 +3,9 @@ package com.LJJStudios.cabinetproject.Item.ingots;
 import com.LJJStudios.cabinetproject.Group.ModGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
 
 public class LeanCoal extends Item {
+
     public LeanCoal(){ super(new Properties().tab(ModGroup.ITEM_GROUP));}
 }
